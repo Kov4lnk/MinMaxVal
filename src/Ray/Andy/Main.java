4279ba64963a55fa064cd95464e80229e050a6e7
Kov4lnk <kov4lnk@gmail.com>
2021-02-01 19:17:59 +0200

    Added min check

--- src/Ray/Andy/Main.java
+++ src/Ray/Andy/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int min = 0;
         int max = 0;
+        boolean first = true;
 
         while (true){
             System.out.println("Enter number:");
@@ -16,6 +17,12 @@ public class Main {
             boolean isAnInt = scanner.hasNextInt();
             if (isAnInt){
                 int number = scanner.nextInt();
+
+                if (first){
+                    first = false;
+                    min = number;
+                    max = number;
+                }
                 if (max < number){
                     max = number;
                 }
